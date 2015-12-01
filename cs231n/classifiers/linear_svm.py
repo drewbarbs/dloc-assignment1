52a82e86fc7f38ebc9d410691d4b7f017586d106
Andrew Barbarello <drewbarbs91@gmail.com>
2015-12-01 11:41:03 -0500

    complete vectorized SVM gradient calculation

--- cs231n/classifiers/linear_svm.py
+++ cs231n/classifiers/linear_svm.py
@@ -51,33 +51,31 @@ def svm_loss_vectorized(W, X, y, reg):
   Inputs and outputs are the same as svm_loss_naive.
   """
   loss = 0.0
-  dW = np.zeros(W.shape) # initialize the gradient as zero
+  num_examples = X.shape[1]
+  num_classes = W.shape[0]
+  col_rng = np.arange(num_examples)
 
-  scores = W.dot(X)
-  raw_loss = (scores - scores[y, np.arange(X.shape[1])]) + 1
-  raw_loss[y, np.arange(X.shape[1])] = 0.0
-  indiv_loss = np.maximum(raw_loss, 0.)
-  data_loss = np.sum(indiv_loss)/X.shape[1] 
+  scores = W.dot(X)  #1
+  raw_loss = (scores - scores[y, col_rng]) + 1 #2
+  raw_loss[y, col_rng] = 0.0 #3
+  indiv_loss = np.maximum(raw_loss, 0.) #4
+  data_loss = np.sum(indiv_loss)/num_examples #5
   reg_loss = 0.5 * reg * np.sum(W**2)
   loss = data_loss + reg_loss
-  #############################################################################
-  # TODO:                                                                     #
-  # Implement a vectorized version of the gradient for the structured SVM     #
-  # loss, storing the result in dW.                                           #
-  #                                                                           #
-  # Hint: Instead of computing the gradient from scratch, it may be easier    #
-  # to reuse some of the intermediate values that you used to compute the     #
-  # loss.                                                                     #
-  #############################################################################
-  d_raw_loss = np.ones(raw_loss.shape)/X.shape[1]
-  d_raw_loss[raw_loss < 0] = 0.0
-  d_scores = W * d_raw_loss
-  d_scores = 1
-  
-  pass
 
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
+  # compute gradient
+
+  d_indiv_loss = np.ones(raw_loss.shape) #5 (divide by num_examples at end)
+  d_raw_loss = d_indiv_loss; d_raw_loss[raw_loss < 0] = 0.0 #4
+  d_scores = d_raw_loss #2 (positive scores term in #2)
+  # for entries of "scores" array corresponding to the correct class,
+  # first set d_scores[entries] = 0 (from #3),
+  # then subtract 1 for each class for which hingle loss was incurred (negative scores term in #2)
+  # combine these two  operations to get the next line
+  d_scores[y, col_rng] = -(raw_loss > 0).sum(axis=0) #3,#2
+  dW = d_scores.dot(X.T) #1
+  dW /= num_examples
+
+  dW += reg*W
 
   return loss, dW
