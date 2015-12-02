1a659c3bf40be5b3233099552cb47c156125e810
Andrew Barbarello <drewbarbs91@gmail.com>
2015-12-02 15:37:51 -0500

    completed softmax.ipynb

--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -18,16 +18,26 @@ def softmax_loss_naive(W, X, y, reg):
   loss = 0.0
   dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+  num_samples = X.shape[1]
+  num_classes = W.shape[0]
+  for i in range(num_samples):
+    scores = W.dot(X[:, i])
+    probs = np.exp(scores)
+    prob_sum = probs.sum()
+    nprob_correct = probs[y[i]]/prob_sum
+    loss += -np.log(nprob_correct)
+    d_nprob_correct = -1./nprob_correct
+    for cls in range(num_classes):
+      if cls == y[i]:
+        dprobs_cls = d_nprob_correct * (prob_sum - probs[y[i]])/prob_sum**2
+      else:
+        dprobs_cls = d_nprob_correct * -probs[y[i]]/prob_sum**2 
+      dscores_cls = dprobs_cls * np.exp(scores[cls])
+      dW[cls, :] += dscores_cls * X[:, i]
+  loss /= num_samples
+  loss += 0.5 * reg * np.sum(W*W)
+  dW /= num_samples
+  dW += reg * W
 
   return loss, dW
 
@@ -38,19 +48,22 @@ def softmax_loss_vectorized(W, X, y, reg):
 
   Inputs and outputs are the same as softmax_loss_naive.
   """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+  num_classes, num_samples = W.shape[0], X.shape[1]
+  col_range = np.arange(num_samples)
+  scores = W.dot(X)
+  probs = np.exp(scores)
+  prob_sum = probs.sum(axis=0)
+  nprob_correct = probs[y, col_range]/prob_sum
+  loss = np.mean(-np.log(nprob_correct))
+  loss += 0.5 * reg * np.sum(W*W)
+  d_nprob_correct = -1./nprob_correct
+  dprobs = np.tile(-probs[y, col_range], (num_classes, 1))
+  dprobs[y, col_range] += prob_sum
+  dprobs *= d_nprob_correct/prob_sum**2
+  dscores = dprobs * probs
+  dW = dscores.dot(X.T) 
+  dW /= num_samples
+  dW += reg * W
 
   return loss, dW
